2fe21a968cba257f43a79d668126cbda73b415aa
TejasMate <tejasmate0@rediffmail.com>
2019-12-20 21:51:52 +0530

    Thanks Program

---
+++ thanks.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include"sampleheader.h"
+
+using namespace std;
+
+void Welcome()  //Function Declaration and Defination
+{
+    cout<<" _______________________________________________________________ "<<endl;
+    cout<<"                        Welcome to GCI                           "<<endl;
+    cout<<" _______________________________________________________________ "<<endl;
+}
